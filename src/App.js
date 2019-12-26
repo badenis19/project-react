cc242f9a98f86eafca81e7c2a7ad3ffc8e119ad8
Bruno ADENIS <adenis.bruno@hotmail.com>
2019-12-26 15:45:25 +0000

    added a component, use of props to display info

--- src/App.js
+++ src/App.js
@@ -9,9 +9,11 @@ import Tweet from "./tweet";
 
 function App(){
   return (
-    <div>
-      <h1>This is the App component</h1>
-      < Tweet />
+    <div className="app">
+      < Tweet name="James Bond" message="Hello" likes="2" />
+      < Tweet name="Christmas" message="Santa" likes="3"/>
+      < Tweet name="Popeye" message="Spinashhhh!" likes="332"/>
+      < Tweet name="Boxing" message="Champion" likes="32"/>
     </div>
   );
 }
--- src/tweet.jsx
+++ src/tweet.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
+// importing the css file
+import "./App.css"
 
-function Tweet(){
+// adding props parameter to the function
+// On App < Tweet name="James Bond" message="Hello" likes="2" />
+// using destructuring the props
+function Tweet({name, message, likes}){
 
     return (
-        <div>
-            <h1>This is the tweet component</h1>
+        <div className="tweet">
+            <h3>{name}</h3>
+            <p>{message}</p>
+            <h3>{likes}</h3>
         </div>
     );
 };
 
 export default Tweet;
+
+
+// OTHER WAY TO GET THE PROPS 
+// function Tweet(props){
+
+//     return (
+//         <div className="tweet">
+//             <h3>{props.name}</h3>
+//             <p>{props.message}</p>
+//             <h3>{props.likes}</h3>
+//         </div>
+//     );
+// };
+
+// export default Tweet;
